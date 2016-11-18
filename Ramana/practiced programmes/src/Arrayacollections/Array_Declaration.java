12a8f6e333c62ea2698eeca51374decfa98d8e25
ramanamuttana <ramanamuttana@gmail.com>
2016-11-18 21:22:35 +0100

    changing the value

--- Ramana/practiced programmes/src/Arrayacollections/Array_Declaration.java
+++ Ramana/practiced programmes/src/Arrayacollections/Array_Declaration.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 public class Array_Declaration {
  static Scanner scan=new Scanner(System.in);
 	public static void main(String[] args) {
-    String[] days= new String[10];
-       
-         for(int i=0;i<10;i++)
+    String[] days= new String[11];
+       // taking other value
+         for(int i=0;i<11;i++)
          {
         	 days[i]=scan.next();
         	 System.out.println(days[i]); 
