76386244d3097c94c7d8937b27dc419418bca347
ramanamuttana <ramanamuttana@gmail.com>
2016-11-18 23:05:23 +0100

    chnaging the value

--- Ramana/practiced programmes/src/Arrayacollections/Array_Declaration.java
+++ Ramana/practiced programmes/src/Arrayacollections/Array_Declaration.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 public class Array_Declaration {
  static Scanner scan=new Scanner(System.in);
 	public static void main(String[] args) {
-    String[] days= new String[11];
+    String[] days= new String[112];
        // taking other value
-         for(int i=0;i<11;i++)
+         for(int i=0;i<112;i++)
          {
         	 days[i]=scan.next();
         	 System.out.println(days[i]); 
