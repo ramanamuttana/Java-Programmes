6d5ec439b5f435cc974855584dc3ffff9e48133d
ramanamuttana <ramanamuttana@gmail.com>
2016-12-16 18:32:00 +0100

    Try and catch exceptions for divison by zero

---
+++ Ramana/practiced programmes/src/exceptions/Try_Catch.java
@@ -0,0 +1,19 @@
+package exceptions;
+
+public class Try_Catch {
+
+	public static void main(String[] args) {
+	 int a=5;
+	 int b=0;
+	 try
+	 {
+		 int c=a/b;
+	 }//here we are mentioning the type of exception 
+	 catch(ArithmeticException e)
+	 {
+		 System.out.println("OOps you cant divide by zero");
+	 }
+
+	}
+
+}
