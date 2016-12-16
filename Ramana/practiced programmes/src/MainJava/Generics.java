f7d402eb999d619f22cf17e2303eb76477128bc0
ramanamuttana <ramanamuttana@gmail.com>
2016-12-16 18:56:52 +0100

    Generics

---
+++ Ramana/practiced programmes/src/MainJava/Generics.java
@@ -0,0 +1,31 @@
+package MainJava;
+import java.util.*;
+
+public class Generics<E> {
+	
+	private LinkedList<E> list=new LinkedList<E> ();
+	
+	E item;
+	public void push(E item)
+	{
+		list.addFirst(item);
+	}
+	public E pop(E item)
+	{
+	   return list.poll();
+	}
+	public E peek(E item)
+	{
+		  return list.peek();
+	}
+	public int size()
+	{
+		return list.size();
+	}
+	public boolean hasItems()
+	{
+		return !list.isEmpty();
+	}
+	
+
+}
