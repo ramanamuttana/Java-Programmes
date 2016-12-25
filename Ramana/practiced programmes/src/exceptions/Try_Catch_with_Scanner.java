ebd0319269c9a317e577c3ca27230b1be8e7e2c5
ramanamuttana <ramanamuttana@gmail.com>
2016-12-25 17:08:32 +0100

    Try and catch with scanner which includes return

---
+++ Ramana/practiced programmes/src/exceptions/Try_Catch_with_Scanner.java
@@ -0,0 +1,28 @@
+package exceptions;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Try_Catch_with_Scanner {
+	
+	 static Scanner scan=new Scanner(System.in);
+
+	public static void main(String[] args)
+	{
+		int i=GetValue();
+		System.out.println("The entered value is :" +i);
+	}
+	public static int  GetValue()
+	{
+		try
+		{
+			return scan.nextInt();
+		}catch(InputMismatchException e)
+		{
+			System.out.println("You enterd value is not an integer");
+			scan.next();
+			return 0;
+		}
+	}
+
+}
