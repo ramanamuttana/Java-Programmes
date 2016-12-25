893bd24a898573d420c67d6cc00b6f1ea1ac9e27
ramanamuttana <ramanamuttana@gmail.com>
2016-12-25 17:25:20 +0100

    Try and catch using while

---
+++ Ramana/practiced programmes/src/exceptions/Try_Catch_With_Scanner_While.java
@@ -0,0 +1,28 @@
+package exceptions;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Try_Catch_With_Scanner_While {
+	static Scanner scan=new Scanner(System.in);
+
+	public static void main(String[] args) 
+	{
+		int i=GetValue();
+		System.out.println("The entered value is :" +i);
+    }
+	public static int  GetValue()
+	{
+	while(true)
+	{
+	try
+	{
+		return scan.nextInt();
+	}catch(InputMismatchException e)
+	{
+		System.out.println("You enterd value is not an integer");
+		scan.next();
+	}
+		return 0;
+	}
+	}
+	}
