168bace168e3b8b316f0005fb5abddfb19aa1796
ramanamuttana <ramanamuttana@gmail.com>
2017-01-01 23:23:57 +0100

    Streams using lambda

--- Ramana/practiced programmes/src/Arrayacollections/Spell.java
+++ Ramana/practiced programmes/src/Arrayacollections/Spell.java
@@ -1,7 +1,6 @@
 package Arrayacollections;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class Spell {
 	
@@ -26,13 +25,13 @@ public class Spell {
 	
 		ArrayList<Spell> spellss=new ArrayList<>();
 		spellss.add(new Spell("Aparecium",Spell.SpellType.SPELL,"Makes invisible ink appear"));
-		spellss.add(new Spell("America",Spell.SpellType.SPELL," It is also same"));
+		spellss.add(new Spell("America",Spell.SpellType.CHARM," It is also same"));
 		spellss.add(new Spell("Singapore",Spell.SpellType.CURSE,"More developed"));
 		/* By using for enhanced loop
 		for(Spell value:spellss)
 			System.out.println(value.name);
         */
-		//here we are using Lamba expressions and filtering only type SPELL
+		//here we are using Lambda expressions and filtering only type SPELL
 		spellss.stream()
 		.filter(s -> s.type==Spell.SpellType.SPELL)
 		.forEach(s -> System.out.println(s));
