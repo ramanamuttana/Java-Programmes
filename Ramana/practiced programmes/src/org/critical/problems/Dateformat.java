c5930ed7e2d7136d7b141adf152dc3d91844e647
ramanamuttana <ramanamuttana@gmail.com>
2017-01-03 16:54:28 +0100

    date format

---
+++ Ramana/practiced programmes/src/org/critical/problems/Dateformat.java
@@ -0,0 +1,23 @@
+package org.critical.problems;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Dateformat {
+
+	public static void main(String[] args) throws ParseException {
+	
+		SimpleDateFormat simpleInput= new SimpleDateFormat("dd/mm/yyyy G");
+		SimpleDateFormat simpleOutput= new SimpleDateFormat("yyyy.mm.dd G");
+		
+		Date date=simpleInput.parse("35/02/2016 AD");
+		System.out.println(date);
+		
+		String output=simpleOutput.format(date);
+		System.out.println(output);
+		
+
+	}
+
+}
