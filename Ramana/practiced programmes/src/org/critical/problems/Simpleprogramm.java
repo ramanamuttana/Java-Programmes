cd58cc42692179b2aafccee8870cbc7dde0d5324
ramanamuttana <ramanamuttana@gmail.com>
2017-01-03 16:55:58 +0100

    simple programm

---
+++ Ramana/practiced programmes/src/org/critical/problems/Simpleprogramm.java
@@ -0,0 +1,13 @@
+package org.critical.problems;
+
+public class Simpleprogramm {
+
+	public static void main(String[] args) 
+	{
+     System.out.println(T.x);
+	}
+}
+class T
+    {
+	public  static final int x=10;
+	}
