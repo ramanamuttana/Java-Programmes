2e1b2e7b743559aba8e1e26a5696aa98e5bfdc63
ramanamuttana <ramanamuttana@gmail.com>
2017-01-27 20:42:16 +0100

    changed the value

--- Ramana/practiced programmes/src/arrayacollections/Hashmap.java
+++ Ramana/practiced programmes/src/arrayacollections/Hashmap.java
@@ -1,12 +1,10 @@
 package arrayacollections;
-
 import java.util.HashMap;
 import java.util.Map;
 
-
 public class Hashmap {
-
-	public static void main(String[] args) {
+public static void main(String[] args) {
+	//hashmap declaration
 		Map<Integer,Integer> Count= new HashMap<Integer,Integer>();
 		Count.put(1000,new Integer(4));
 		Count.put(1002,new Integer(3));
@@ -14,14 +12,12 @@ public class Hashmap {
 		Count.put(1003,new Integer(5));
 		Count.put(1004,new Integer(2));
 		//System.out.println(Count);
+		//using enhanced for loop and getting the keys and values 
 		for(Map.Entry<Integer, Integer> entry :Count.entrySet())
 		{
-			Integer Key =entry.getKey();
-		 Integer vampire=entry.getValue();
-		 System.out.println(Key+" "+vampire);
-		 
+		Integer Key =entry.getKey();
+		Integer number=entry.getValue();
+		System.out.println(Key+" "+number); 
 		}
-
 	}
-
 }
