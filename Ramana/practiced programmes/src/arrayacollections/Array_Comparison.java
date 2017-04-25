68c0bf664f2328222f5349f5b3b8b009285fc159
ramanamuttana <ramanamuttana@gmail.com>
2017-04-25 22:13:40 +0200

    i changed

--- Ramana/practiced programmes/src/arrayacollections/Array_Comparison.java
+++ Ramana/practiced programmes/src/arrayacollections/Array_Comparison.java
@@ -1,24 +1,24 @@
 package arrayacollections;
-
 import java.util.Arrays;
 
 public class Array_Comparison {
-			public static void main(String[] args) {
+	
+			public static void main(String[] args) 
+			{
 		    String[] days= new String[3];
 		    days[0]="Sunday";
 		    days[1]="monday";
-		    days[2]="tuesda";
+		    days[2]="tuesday";
 		    String[] day= new String[3];
 		    day[0]="Sunday";
 		    day[1]="monday";
 		    day[2]="tuesday";
 		    if(Arrays.equals(days, day))
 		    {
-		        	 System.out.println("They are equal"); 
+		         System.out.println("They are equal jabed"); 
 		     }else
 		     {
 		    	 System.out.println("They are not equal"); 
 		     }
-
 			}
 }
