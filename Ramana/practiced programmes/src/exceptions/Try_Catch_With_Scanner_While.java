9e3a9a6b9e8c40c8bd10751c107e09611bcd45fb
ramanamuttana <ramanamuttana@gmail.com>
2017-10-15 16:16:09 +0200

    Defifning the data type to enter

--- Ramana/practiced programmes/src/exceptions/Try_Catch_With_Scanner_While.java
+++ Ramana/practiced programmes/src/exceptions/Try_Catch_With_Scanner_While.java
@@ -1,28 +1,26 @@
 package exceptions;
+
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Try_Catch_With_Scanner_While {
-	static Scanner scan=new Scanner(System.in);
+	static Scanner scan = new Scanner(System.in);
 
-	public static void main(String[] args) 
-	{
-		int i=GetValue();
-		System.out.println("The entered value is :" +i);
-    }
-	public static int  GetValue()
-	{
-	while(true)
-	{
-	try
-	{
-		return scan.nextInt();
-	}catch(InputMismatchException e)
-	{
-		System.out.println("You enterd value is not an integer");
-		scan.next();
+	public static void main(String[] args) {
+		// It takes only integers if we give string we will get mismatch exception 
+		int i = GetValue();
+		System.out.println("The entered value is :" + i);
 	}
-		return 0;
+
+	public static int GetValue() {
+		while (true) {
+			try {
+				return scan.nextInt();
+			} catch (InputMismatchException e) {
+				System.out.println("You enterd value is not an integer");
+				scan.next();
+			}
+			return 0;
+		}
 	}
-	}
-	}
+}
