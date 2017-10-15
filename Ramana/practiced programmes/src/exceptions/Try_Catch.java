94789b46d544f14c6d2a2080983f7de6a7f569a7
ramanamuttana <ramanamuttana@gmail.com>
2017-10-15 18:22:24 +0200

    formatting

--- Ramana/practiced programmes/src/exceptions/Try_Catch.java
+++ Ramana/practiced programmes/src/exceptions/Try_Catch.java
@@ -3,16 +3,14 @@ package exceptions;
 public class Try_Catch {
 
 	public static void main(String[] args) {
-	 int a=5;
-	 int b=0;
-	 try
-	 {
-		 int d=a/b;
-	 }//here we are mentioning the type of exception 
-	 catch(ArithmeticException e)
-	 {
-		 System.out.println("OOps you cant divide by zero");
-	 }
+		int a = 5;
+		int b = 0;
+		try {
+			int d = a / b;
+		} // here we are mentioning the type of exception
+		catch (ArithmeticException e) {
+			System.out.println("OOps you cant divide by zero");
+		}
 
 	}
 
