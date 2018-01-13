70ae058365565a8a6cedcdf07b9f797be2348ef9
ramanamuttana <ramanamuttana@gmail.com>
2018-01-13 19:40:11 +0100

    Comparing Arrays of differnt sizes

--- Ramana/practiced programmes/src/arrayacollections/Array_Comparison.java
+++ Ramana/practiced programmes/src/arrayacollections/Array_Comparison.java
@@ -1,24 +1,26 @@
 package arrayacollections;
+
 import java.util.Arrays;
 
 public class Array_Comparison {
-	
-	public static void main(String[] args){
-				
-	  String[] days= new String[3];
-	        days[0]="Sunday";
-		    days[1]="monday";
-		    days[2]="tuesday";
-	  String[] day= new String[3];
-		    day[0]="Sunday";
-		    day[1]="monday";
-		    day[2]="tuesday";
-		    
-	 if(Arrays.equals(days, day))
-	    {
-		    System.out.println("They are equal"); 
-		}else{
-		    System.out.println("They are not equal"); 
+
+	public static void main(String[] args) {
+
+		//Comparing arrays having Strings
+		String[] days = new String[3];
+		days[0] = "Sunday";
+		days[1] = "monday";
+		days[2] = "tuesday";
+		String[] day = new String[4];
+		day[0] = "Sunday";
+		day[1] = "monday";
+		day[2] = "tuesday";
+		day[3] = "wednesday";
+
+		if (Arrays.equals(days, day)) {
+			System.out.println("They are equal");
+		} else {
+			System.out.println("They are not equal");
 		}
-	    }
+	}
 }
