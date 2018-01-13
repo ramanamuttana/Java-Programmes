b689e4b981768381693ac1e6f19e994ab1620b88
ramanamuttana <ramanamuttana@gmail.com>
2018-01-13 20:09:46 +0100

    Taking elements from scanner

---
+++ Ramana/practiced programmes/src/arrayacollections/ArrayScanner.java
@@ -0,0 +1,19 @@
+package arrayacollections;
+
+import java.util.Scanner;
+
+public class ArrayScanner {
+	
+	static Scanner scan = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		// declaring the size of the array
+		String[] days = new String[112];
+		// taking value
+		for (int i = 0; i < days.length; i++) {
+			days[i] = scan.next();
+			System.out.println(days[i]);
+		}
+
+	}
+}
