6bb1c9596d3667e4027a5b94674bc8b8e598c9d6
ramanamuttana <ramanamuttana@gmail.com>
2018-07-06 21:52:03 +0200

    scanner class

---
+++ Ramana/practiced programmes/Part1/Decisionlopps/Scannerclose.java
@@ -0,0 +1,25 @@
+package Decisionlopps;
+
+import java.util.*;
+
+public class Scannerclose {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		scan.close();
+		String ans =null;
+
+		// if 'n' is NOT evenly divisible by 2 (i.e.: n is odd)
+		if (n % 2 == 1) {
+			ans = "oddNumber ";
+		} else if ((n % 2 == 0) && (n > 1 && n <= 5)) {
+			ans = "n may be 2 or 4";
+		} else if ((n % 2 == 0) && (n > 5 && n <= 20)) {
+			ans = "even numbers between 5 and 20";
+		} else if ((n % 2 == 0) && (n > 20 && n <= 100)) {
+			ans = "even numbers between 20 and 100";
+		}
+		System.out.println(ans);
+	}
+}
