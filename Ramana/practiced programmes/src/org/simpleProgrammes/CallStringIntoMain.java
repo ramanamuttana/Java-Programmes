1f8b23f26de1cd5a1a5bd7a45403c6656b7ecd9b
ramanamuttana <ramanamuttana@gmail.com>
2018-07-06 21:08:01 +0200

    calling the static variable into the main method

---
+++ Ramana/practiced programmes/src/org/simpleProgrammes/CallStringIntoMain.java
@@ -0,0 +1,13 @@
+package org.simpleProgrammes;
+
+public class CallStringIntoMain {
+
+	static String value = "HelloWorld";
+
+	// calling the static String variable into main method
+	public static void main(String[] args) {
+
+		System.out.println(value);
+	}
+
+}
