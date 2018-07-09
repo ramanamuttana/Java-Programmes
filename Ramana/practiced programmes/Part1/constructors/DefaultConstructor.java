6bbbb204539900f528ef6ffb0a31aeeb6a1d43f5
ramanamuttana <ramanamuttana@gmail.com>
2018-07-09 19:31:40 +0200

    default constructor

---
+++ Ramana/practiced programmes/Part1/constructors/DefaultConstructor.java
@@ -0,0 +1,13 @@
+package constructors;
+
+public class DefaultConstructor {
+
+	// now we have default constructor 
+	// object is created through the default constructor 
+	public static void main(String[] args) {
+
+		DefaultConstructor d1 = new DefaultConstructor();
+
+	}
+
+}
