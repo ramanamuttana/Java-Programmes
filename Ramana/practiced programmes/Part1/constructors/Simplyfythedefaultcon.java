fe16072fe98b4b35078c52522f2bfe91ed30d91a
ramanamuttana <ramanamuttana@gmail.com>
2018-07-09 19:52:08 +0200

    no default constructor

---
+++ Ramana/practiced programmes/Part1/constructors/Simplyfythedefaultcon.java
@@ -0,0 +1,27 @@
+package constructors;
+
+public class Simplyfythedefaultcon {
+
+	int x, y;
+
+	public Simplyfythedefaultcon(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public static void main(String[] args) {
+		// no default constructor and setting the variable only through
+		// constructor
+		Simplyfythedefaultcon def2 = new Simplyfythedefaultcon(10, 30);
+		System.out.println(def2.getX());
+	}
+
+}
