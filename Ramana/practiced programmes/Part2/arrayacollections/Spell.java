59e4f7055ade738ed7d4e6bba84726a0d8d289ab
venkataramanareddy <venkataramanareddy@Lenovo-PC.Speedport_W723_V_Typ_A_1_01_018>
2018-07-31 23:52:25 +0200

    formatted to clean code

--- Ramana/practiced programmes/Part2/arrayacollections/Spell.java
+++ Ramana/practiced programmes/Part2/arrayacollections/Spell.java
@@ -3,38 +3,37 @@ package arrayacollections;
 import java.util.ArrayList;
 
 public class Spell {
-	
+
 	public String name;
 	public SpellType type;
 	public String desc;
-	public enum SpellType {SPELL,CHARM,CURSE}
-	
-	public Spell(String spellName,SpellType spell,String description)
-	{
-		name=spellName;
-		type=spell;
-		desc=description;
+
+	public enum SpellType {
+		SPELL, CHARM, CURSE
 	}
-	public String toString()
-	{
+
+	public Spell(String spellName, SpellType spell, String description) {
+		name = spellName;
+		type = spell;
+		desc = description;
+	}
+
+	public String toString() {
 		return name;
 	}
 
-	public static void main(String[] args) 
-	{
-	
-		ArrayList<Spell> spellss=new ArrayList<>();
-		spellss.add(new Spell("Aparecium",Spell.SpellType.SPELL,"Makes invisible ink appear"));
-		spellss.add(new Spell("America",Spell.SpellType.CHARM," It is also same"));
-		spellss.add(new Spell("Singapore",Spell.SpellType.CURSE,"More developed"));
-		/* By using for enhanced loop
-		for(Spell value:spellss)
-			System.out.println(value.name);
-        */
-		//here we are using Lambda expressions and filtering only type SPELL
-		spellss.stream()
-		.filter(s -> s.type==Spell.SpellType.SPELL)
-		.forEach(s -> System.out.println(s));
+	public static void main(String[] args) {
+
+		ArrayList<Spell> spellss = new ArrayList<>();
+		spellss.add(new Spell("Aparecium", Spell.SpellType.SPELL, "Makes invisible ink appear"));
+		spellss.add(new Spell("America", Spell.SpellType.CHARM, " It is also same"));
+		spellss.add(new Spell("Singapore", Spell.SpellType.CURSE, "More developed"));
+		/*
+		 * By using for enhanced loop for(Spell value:spellss)
+		 * System.out.println(value.name);
+		 */
+		// here we are using Lambda expressions and filtering only type SPELL
+		spellss.stream().filter(s -> s.type == Spell.SpellType.SPELL).forEach(s -> System.out.println(s));
 	}
 
 }
