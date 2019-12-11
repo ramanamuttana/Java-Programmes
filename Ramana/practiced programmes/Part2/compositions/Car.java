d21ff45123d0072744c4e54f6504305e9a92fb17
ramanamuttana <ramanamuttana@gmail.com>
2019-12-11 17:19:20 +0100

    Update Car.java

--- Ramana/practiced programmes/Part2/compositions/Car.java
+++ Ramana/practiced programmes/Part2/compositions/Car.java
@@ -1,28 +1,27 @@
 package compositions;
 
 public class Car {
-	
+
 	private int nofoTyres;
 	private String carName;
 	private Engine engine;
-	
-	public  Car(int tyres,String nameofthecar,Engine enginevalue )
-	{
-		 this.nofoTyres=tyres;
-		 this.carName=nameofthecar;
-		 this.engine=enginevalue;
-	}
-	public String toString()
-	{
-		return String.format("%d %s %s",nofoTyres, carName, engine);
+
+	public Car(int tyres, String nameofthecar, Engine enginevalue) {
+		this.nofoTyres = tyres;
+		this.carName = nameofthecar;
+		this.engine = enginevalue;
 	}
 
-	public static void main(String[] args)
-	{
-		//we are creating object for Engine class
-		Engine eng=new Engine("audiiis3","medium","fast");
-           Car c = new Car(4,"Audii",eng);
-            System.out.println(c);
+	public String toString() {
+		return String.format("%d %s %s", nofoTyres, carName, engine);
 	}
 
+	// sending whole object to other class , This kind of example is required
+	// especially when employee object is there .
+	public static void main(String[] args) {
+		// we are creating object for Engine class
+		Engine eng = new Engine("audiiis3", "medium", "fast");
+		Car c = new Car(4, "Audii", eng);
+		System.out.println(c);
+	}
 }
