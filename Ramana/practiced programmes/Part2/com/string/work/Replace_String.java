34d1c7c9e7f18811f93f912df39dd011a0f2bee0
ramanamuttana <ramanamuttana@gmail.com>
2019-12-11 17:24:51 +0100

    Update Replace_String.java

--- Ramana/practiced programmes/Part2/com/string/work/Replace_String.java
+++ Ramana/practiced programmes/Part2/com/string/work/Replace_String.java
@@ -1,16 +1,16 @@
-package com.string.work;
+package StringChanges;
 
 import java.util.Scanner;
 
-public class Replace_String 
-            {
-			static Scanner scan=new Scanner(System.in);
-			public static void main(String[] args)
-			{
-				System.out.println("enter some string");
-			    String sub=scan.nextLine();
-			    //sub=sub.replaceAll("cat", "dog");
-			    sub=sub.replaceFirst("vishnu", "dog");
-			    	System.out.println(sub);
-			    }
-	        }
+public class Replace_String {
+	
+static Scanner scan = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		System.out.println("enter some string");
+		String sub = scan.nextLine();
+		// sub=sub.replaceAll("cat", "dog");
+		sub = sub.replaceFirst("vish", "new guy");
+		System.out.println(sub);
+	}
+}
