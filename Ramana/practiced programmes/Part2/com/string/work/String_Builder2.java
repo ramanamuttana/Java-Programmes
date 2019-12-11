a84aa4eaf32baf3809cf64bbb961fc60c07a49f5
ramanamuttana <ramanamuttana@gmail.com>
2019-12-11 17:23:15 +0100

    Update String_Builder2.java

--- Ramana/practiced programmes/Part2/com/string/work/String_Builder2.java
+++ Ramana/practiced programmes/Part2/com/string/work/String_Builder2.java
@@ -1,33 +1,23 @@
-package com.string.work;
+package StringChanges;
 
-public class String_Builder2 
-{
-	String step1="jabed";
-	public String_Builder2(String string) {
-		// TODO Auto-generated constructor stub
+public class String_Builder2 {
+	String step1 = "johnson";
+
+  // thie example is without creating an variable 
+	public static void wrapp(String stem1) {
+		stem1 = stem1 + "first candidate";
 	}
-	public static void wrapp(String stem1)
-	{
-		stem1=stem1+"first candidate";
-	}
-	public static void wrapp2(StringBuilder sub)
-	{
+
+	public static void wrapp2(StringBuilder sub) {
 		sub.append("first candidate");
 	}
-	
-	private void append(String string) {
-		// TODO Auto-generated method stub
-		
-	}
-	public static void main(String[] args)
-	{
-		 String s1="jabed";
-		 wrapp(s1);
-		 System.out.println(s1);
-		 StringBuilder sub=new StringBuilder("jabedsirji ");
-		 wrapp2(sub);
-		 System.out.println(sub);
-		 
-	}
 
+	public static void main(String[] args) {
+		String s1 = "johnson";
+		wrapp(s1);
+		System.out.println(s1);
+		StringBuilder sub = new StringBuilder("jabedsirji ");
+		wrapp2(sub);
+		System.out.println(sub);
+	}
 }
