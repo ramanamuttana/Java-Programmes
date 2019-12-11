39cdcde4a0f650d66a4285c6d1f2d0cc3c25ab79
ramanamuttana <ramanamuttana@gmail.com>
2019-12-11 17:22:04 +0100

    Update String_Buolder.java

--- Ramana/practiced programmes/Part2/com/string/work/String_Buolder.java
+++ Ramana/practiced programmes/Part2/com/string/work/String_Buolder.java
@@ -1,26 +1,33 @@
-package com.string.work;
+package StringChanges;
 
 import java.util.Scanner;
 
 public class String_Buolder {
-	 static Scanner scan=new Scanner(System.in);
+	static Scanner scan = new Scanner(System.in);
 
+	// changing wach character to
 	public static void main(String[] args) {
-					System.out.println("enter some string");
-				    String sub=scan.nextLine();
-				    StringBuilder sb=new StringBuilder(sub);
-						 int Vowelcount=0;
-						for(int i=0;i<sub.length();i++)
-						{
-							char c=sub.charAt(i);
-							if((c=='A')||(c=='a')||(c=='E')||(c=='e')||(c=='I')||(c=='i')||(c=='O')||(c=='o')||(c=='u')||(c=='U'))
-							{
-								Vowelcount++;
-							}
-							sb.setCharAt(i, '#');
-						}
-						System.out.println(" There are  "+Vowelcount+" vowels in given sentence");
-						System.out.println(sub);
-						System.out.println(sb.toString());
-					    }
+		System.out.println("enter some string");
+		String sub = scan.nextLine();
+		StringBuilder sb = new StringBuilder(sub);
+		int Vowelcount = 0;
+		for (int i = 0; i < sub.length(); i++) {
+			char c = sub.charAt(i);
+			if ((c == 'A') || (c == 'a') || (c == 'E') || (c == 'e') || (c == 'I') || (c == 'i') || (c == 'O')
+					|| (c == 'o') || (c == 'u') || (c == 'U')) {
+				Vowelcount++;
+			}
+			sb.setCharAt(i, '#');
+		}
+		System.out.println(" There are  " + Vowelcount + " vowels in given sentence");
+		System.out.println(sub);
+		System.out.println(sb.toString());
+
+		// the output 
+		/*
+		 * enter some string, the berlin is the capital of germany ,There are 11 vowels in
+		 * given sentence, the berlin is the capital of germany,
+		 * ######################################
+		 */
 	}
+}
