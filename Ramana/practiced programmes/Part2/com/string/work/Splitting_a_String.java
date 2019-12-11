ecebfbeefc2fa772f1f74601e5b87a224dc460ca
ramanamuttana <ramanamuttana@gmail.com>
2019-12-11 17:23:58 +0100

    Update Splitting_a_String.java

--- Ramana/practiced programmes/Part2/com/string/work/Splitting_a_String.java
+++ Ramana/practiced programmes/Part2/com/string/work/Splitting_a_String.java
@@ -1,18 +1,16 @@
-package com.string.work;
+package StringChanges;
+
 import java.util.Scanner;
 
 public class Splitting_a_String {
-	 static Scanner scan=new Scanner(System.in);
-	public static void main(String[] args)
-	{
-		System.out.println("enter some string");
-	    String sub=scan.nextLine();
-	    String[] w=sub.split("\\s+");
-	    for(String semi:w)
-	    {
-	    	System.out.println(semi);
-	    }
-	
-	 }
-	}
+	static Scanner scan = new Scanner(System.in);
 
+	public static void main(String[] args) {
+		System.out.println("enter some string");
+		String sub = scan.nextLine();
+		String[] w = sub.split("\\s+");
+		for (String semi : w) {
+			System.out.println(semi);
+		}
+	}
+}
