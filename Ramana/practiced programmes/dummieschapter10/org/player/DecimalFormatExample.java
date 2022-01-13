0ac4d31767df542e6e711d14ca5f131b5df14fd2
ramanamuttana <ramanamuttana@gmail.com>
2022-01-13 22:34:57 +0100

    changes to Student class

--- Ramana/practiced programmes/dummieschapter10/org/player/DecimalFormatExample.java
+++ Ramana/practiced programmes/dummieschapter10/org/player/DecimalFormatExample.java
@@ -5,11 +5,17 @@ import java.text.DecimalFormat;
 public class DecimalFormatExample {
 	
 	public static void main(String args[ ]) {
-		DecimalFormat decFormat=new DecimalFormat();
-		decFormat.setMinimumFractionDigits(2);
-		decFormat.setNegativePrefix("("); // before negative value add (
-		decFormat.setNegativeSuffix(")"); // before negative value add last )
-		System.out.println(decFormat.format(345));
-		System.out.println(decFormat.format(-345));
+	Student ramesh=new Student();
+	ramesh.setSubject("Telugu");
+	ramesh.setMarks(89);
+	Student venkatesh= new Student();
+	venkatesh.setSubject("Telugu");
+	venkatesh.setMarks(86);
+	
+	
+	System.out.println("ramesh marks : "+ramesh.getSubjectMarks());
+	System.out.println("Venkatesh Marks : "+venkatesh.getSubjectMarks());
+	
+	
 	}
 }
---
+++ Ramana/practiced programmes/dummieschapter10/org/player/Student.java
@@ -0,0 +1,42 @@
+package org.player;
+
+import java.text.DecimalFormat;
+
+public class Student{
+	
+	private String subject;
+	private int marks;
+	
+	
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public int getMarks() {
+		return marks;
+	}
+
+	public void setMarks(int marks) {
+		this.marks = marks;
+	}
+
+	public String getSubjectName(){
+		return subject;
+	}
+	
+	public int getSubjectMarks() {
+		return marks;
+	}
+	
+	public String getAverageString() {
+		DecimalFormat decFormat=new DecimalFormat();
+		decFormat.setMaximumFractionDigits(0);
+		decFormat.setMaximumFractionDigits(3);
+		decFormat.setMinimumFractionDigits(3);
+		return decFormat.format(marks);
+	}
+}
