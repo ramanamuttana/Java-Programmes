6d55a6bdd9af2412b111497f1a8c74fa740149b1
ramanamuttana <ramanamuttana@gmail.com>
2022-01-13 22:07:46 +0100

    Updated Decimalformat

--- Ramana/practiced programmes/dummieschapter10/org/player/DecimalFormatExample.java
+++ Ramana/practiced programmes/dummieschapter10/org/player/DecimalFormatExample.java
@@ -7,8 +7,8 @@ public class DecimalFormatExample {
 	public static void main(String args[ ]) {
 		DecimalFormat decFormat=new DecimalFormat();
 		decFormat.setMinimumFractionDigits(2);
-		decFormat.setNegativePrefix("(");
-		decFormat.setNegativeSuffix(")");
+		decFormat.setNegativePrefix("("); // before negative value add (
+		decFormat.setNegativeSuffix(")"); // before negative value add last )
 		System.out.println(decFormat.format(345));
 		System.out.println(decFormat.format(-345));
 	}
