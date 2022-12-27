0b9414221c24e245fd69bb33645b00d7337bb403
ramanamuttana <ramanamuttana@gmail.com>
2022-12-27 23:26:51 +0100

    added a comment in Part 1 car2 class

--- Ramana/practiced programmes/Part1/constructors/Car2.java
+++ Ramana/practiced programmes/Part1/constructors/Car2.java
@@ -4,6 +4,6 @@ public class Car2 extends Car {
 
 	public static void main(String[] args) {
 		Car cga = new Car(10);
-		System.out.println(p);
+		System.out.println(p); // since we are extending the car class , then we no need to call classname .p 
 	}
 }
