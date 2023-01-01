c1a3169b3d82e103849acabb96072ee4c7792fdd
ramanamuttana <ramanamuttana@gmail.com>
2023-01-01 20:58:31 +0100

    Problem 81 created
    explicit literals for byte and for short

---
+++ Ramana/practiced programmes/javacodingproblems/org/chapter4/Problem81.java
@@ -0,0 +1,17 @@
+package org.chapter4;
+
+public class Problem81 {
+	
+	public static void main(String[] args) {
+		
+		// both are not considered as byte and short as per request 
+	//	var byteNumer=25;
+	//	var shortNumber=1463;
+		
+		var byteNumber=(byte)25;
+		var shortNumber=(short)1463;
+		
+		System.out.println(byteNumber);
+		System.out.println(shortNumber);
+	}
+}
