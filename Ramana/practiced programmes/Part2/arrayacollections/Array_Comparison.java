f337a1334a9a4f4d13035fc2fac905ffbe9348fb
ramanamuttana <ramanamuttana@gmail.com>
2023-01-05 19:19:31 +0100

    small update in part2

--- Ramana/practiced programmes/Part2/arrayacollections/Array_Comparison.java
+++ Ramana/practiced programmes/Part2/arrayacollections/Array_Comparison.java
@@ -3,6 +3,8 @@ package arrayacollections;
 import java.util.Arrays;
 
 public class Array_Comparison {
+	
+	static String weekend="Sunday";
 
 	public static void main(String[] args) {
 
@@ -16,11 +18,35 @@ public class Array_Comparison {
 		day[1] = "monday";
 		day[2] = "tuesday";
 		day[3] = "wednesday";
+		
+		String[] oftenday = new String[1];
+		oftenday[0] = "Sunday";
 
+		// comparision of values 
 		if (Arrays.equals(days, day)) {
 			System.out.println("They are equal");
 		} else {
 			System.out.println("They are not equal");
 		}
+		
+		/**
+		 * only values are compared not String variable and Object 
+		 */
+		
+		if (oftenday[0]==weekend) {
+			System.out.println("They are equal");
+		} else {
+			System.out.println("They are not equal");
+		}
+		
 	}
 }
+
+/*
+ * package org.example; public class Main {
+ *  public static void main(String[] args) { 
+ *  String str1="Test"; 
+ *  String str2=new String("Test");
+ * System.out.println(str1==str2); // false
+ * System.out.println(str1.equals(str2)); // true } }
+ */
