d32e0c98a809629e19fb593db5fe3fc12eefa277
ramanamuttana <ramanamuttana@gmail.com>
2024-05-11 12:39:23 +0200

    Update DefaultConstructor.java

--- Ramana/practiced programmes/Part1/constructors/DefaultConstructor.java
+++ Ramana/practiced programmes/Part1/constructors/DefaultConstructor.java
@@ -4,6 +4,7 @@ public class DefaultConstructor {
 
 	// now we have default constructor 
 	// object is created through the default constructor 
+	// Note:when ever we add some arguements to constructor then , the default constructor wont be available .
 	public static void main(String[] args) {
 
 		DefaultConstructor d1 = new DefaultConstructor();
