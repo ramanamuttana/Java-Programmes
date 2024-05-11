394cb5b06a865520e7a842c653256a2d7bf7d78a
ramanamuttana <ramanamuttana@gmail.com>
2024-05-11 12:32:40 +0200

    Update Car.java

--- Ramana/practiced programmes/Part1/constructors/Car.java
+++ Ramana/practiced programmes/Part1/constructors/Car.java
@@ -2,27 +2,27 @@ package constructors;
 
 public class Car {
 	
+    // this is static variable so it can be accessed through the class name 
+    public static int p;  
 
-	public static int p;  // this is static variable so it can be accessed through class name 
+    /**
+     * default constructor will be there but when we write a constructor with an 
+     * arguement then the default constructor will not be there.
+     * incase if we want the default constructor , then we need to type manually .
+     */
+    public Car() {
 
-	/**
-	 * default constructor will be there but when we write a constructor with an 
-	 * arguement then the default constructor will not be there.
-	 * incase if we want the default constructor , then we need to type manually .
-	 */
-	public Car() {
+    }
 
-	}
+    public Car(int x) {
+	this.p = x;
+     }
 
-	public Car(int x) {
-		this.p = x;
-	}
+     public Car(int h, int x) {
 
-	public Car(int h, int x) {
-
-	}
+     }
 	
-	public void getWorld() {
-		System.out.println("Hello world");
-	}
+      public void getWorld() {
+	 System.out.println("Hello world");
+      }
 }
