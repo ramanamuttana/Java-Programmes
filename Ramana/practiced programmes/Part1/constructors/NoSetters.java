47c0c49b232783b57ebbaa7b89a105beba79d3f3
ramanamuttana <ramanamuttana@gmail.com>
2024-05-11 12:54:22 +0200

    Update NoSetters.java

--- Ramana/practiced programmes/Part1/constructors/NoSetters.java
+++ Ramana/practiced programmes/Part1/constructors/NoSetters.java
@@ -1,16 +1,18 @@
 package constructors;
 
-public class NoSetters {
+public class Nosetters {
 
-	public static void main(String[] args) {
-		Databses dbs = new Databses(10, 20);
-		System.out.println("One value is: " + dbs.getX() + " the other value is: " + dbs.getY());
-	}
+public static void main(String[] args) {
+   // we are setting the values through the constructor 
+   Databses dbs = new Databses(10, 20);
+   System.out.println("One value is: " + dbs.getX() + " the other value is: " + dbs.getY());
+}
 }
 
 /*
  * when there are no setter methods then it will be difficult to modify the values 
  * ONly the case the possible is if all members are public including variables .
+ * or if constructor is having the varibales as a arguements .
  * 
  */
 
