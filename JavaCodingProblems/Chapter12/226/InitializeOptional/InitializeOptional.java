c58eb8247f0cc9f0b4a83aaf3903add8fa14d7ed
ramanamuttana <ramanamuttana@gmail.com>
2024-05-14 14:07:49 +0200

    Update InitializeOptional.java

--- JavaCodingProblems/Chapter12/226/InitializeOptional/InitializeOptional.java
+++ JavaCodingProblems/Chapter12/226/InitializeOptional/InitializeOptional.java
@@ -1,1 +1,17 @@
+package org.example;
 
+import java.util.Optional;
+
+public class Initializeoptional {
+    public static void main(String[] args) {
+        // Example 226
+        // Avoid , not the right way to initialize
+        Optional<Book> bookAvoid=null;
+
+        // the correct way to initialize is
+        Optional<Book> bookPrefered = Optional.empty();
+
+        System.out.println("Hello world!");
+
+    }
+}
