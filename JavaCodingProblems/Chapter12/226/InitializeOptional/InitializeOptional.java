205bf08cdddd7e67ccadf2cc0af404b202b5d233
ramanamuttana <ramanamuttana@gmail.com>
2024-05-14 14:10:03 +0200

    Update InitializeOptional.java

--- JavaCodingProblems/Chapter12/226/InitializeOptional/InitializeOptional.java
+++ JavaCodingProblems/Chapter12/226/InitializeOptional/InitializeOptional.java
@@ -3,15 +3,14 @@ package org.example;
 import java.util.Optional;
 
 public class Initializeoptional {
+    
     public static void main(String[] args) {
+        
         // Example 226
-        // Avoid , not the right way to initialize
+        // Avoid , not the right way to initialize, Since Optional acts as a Container , so avoid initializing with NULL value 
         Optional<Book> bookAvoid=null;
 
         // the correct way to initialize is
         Optional<Book> bookPrefered = Optional.empty();
-
-        System.out.println("Hello world!");
-
     }
 }
