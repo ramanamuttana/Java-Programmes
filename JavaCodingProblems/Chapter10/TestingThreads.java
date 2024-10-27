f8c12354f431a45605febfd6b53223d96b3c07e6
ramanamuttana <ramanamuttana@gmail.com>
2024-10-27 16:18:44 +0100

    Update TestingThreads.java

--- JavaCodingProblems/Chapter10/TestingThreads.java
+++ JavaCodingProblems/Chapter10/TestingThreads.java
@@ -1,1 +1,15 @@
+package org.example;
 
+public class TestingThreads {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        TestingThreads tthreads =new TestingThreads();
+        tthreads.runnableThreads();
+    }
+
+    public void runnableThreads(){
+        Thread t1=new Thread(()->{});
+        System.out.println(t1.getState()+"  Thread State ");
+    }
+}
