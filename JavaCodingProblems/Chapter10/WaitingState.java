e0a3ca43e018f211bd9bfdc7e81e99c7ec4b04e2
ramanamuttana <ramanamuttana@gmail.com>
2024-10-27 17:07:44 +0100

    Update WaitingState.java

--- JavaCodingProblems/Chapter10/WaitingState.java
+++ JavaCodingProblems/Chapter10/WaitingState.java
@@ -1,1 +1,26 @@
+package org.example;
+
+public class WaitingThreads {
+
+    public static void main(String[] args ){
+        new Thread(() -> {
+            Thread t1 = Thread.currentThread();
+            Thread t2 = new Thread(() -> {
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println("WaitingThread t1: "
+                        + t1.getState()); // WAITING
+            });
+            t2.start();
+            try {
+                t2.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }).start();
+    }
+}
 
