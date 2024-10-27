7f5c0c433a32eb627906f24d55ee77f3ed5f61f8
ramanamuttana <ramanamuttana@gmail.com>
2024-10-27 16:21:00 +0100

    Update RunnableThreadState.java

--- JavaCodingProblems/Chapter10/RunnableThreadState.java
+++ JavaCodingProblems/Chapter10/RunnableThreadState.java
@@ -1,1 +1,17 @@
+package org.example;
 
+public class RunnableThreadState {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        RunnableThreadState tthreads =new RunnableThreadState();
+        tthreads.runnableThreads();
+    }
+
+    public void runnableThreads(){
+        Thread t1=new Thread(()->{});
+        t1.start();
+        System.out.println(t1.getState()+"  Thread State ");
+    }
+}
