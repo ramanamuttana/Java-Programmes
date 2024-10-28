898176e94574bf9eb97ee36cacd14fd6c0b03dc6
ramanamuttana <ramanamuttana@gmail.com>
2024-10-28 10:56:07 +0100

    Update LockingAtObjectLevel.java

--- JavaCodingProblems/Chapter10/LockingAtObjectLevel.java
+++ JavaCodingProblems/Chapter10/LockingAtObjectLevel.java
@@ -1,1 +1,49 @@
+package org.example;
 
+public class LockingAtObjectLevel {
+    public static void main(String[] args) {
+
+        LockingAtObjectLevel  testingThreads= new LockingAtObjectLevel ();
+        TestingAboveClass testingAboveClass=new TestingAboveClass( testingThreads);
+        TestingAboveClass2 testingAboveClass2=new TestingAboveClass2( testingThreads);
+        testingAboveClass.start();
+        testingAboveClass2.start();
+
+    }
+    public synchronized void m1(){
+        System.out.println("in Method m1 the thread name is :"+Thread.currentThread().getName());
+    }
+
+    public synchronized void m2(){
+        System.out.println("in Method m2 the thread name is :"+Thread.currentThread().getName());
+    }
+}
+
+class TestingAboveClass extends Thread{
+   LockingAtObjectLevel  testingThreads=null;
+    TestingAboveClass(LockingAtObjectLevel  testingThreads){
+        this.testingThreads=testingThreads;
+    }
+
+    @Override
+    public void run(){
+        testingThreads.m1();
+        testingThreads.m2();
+    }
+
+}
+
+class TestingAboveClass2 extends Thread{
+
+   LockingAtObjectLevel  testingThreads=null;
+    TestingAboveClass2(LockingAtObjectLevel  testingThreads){
+        this.testingThreads=testingThreads;
+    }
+
+    @Override
+    public void run(){
+        testingThreads.m1();
+        testingThreads.m2();
+    }
+
+}
