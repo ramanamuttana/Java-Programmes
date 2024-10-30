53146d06dad490edde80ffc6aeb2df4428f36986
ramanamuttana <ramanamuttana@gmail.com>
2024-10-30 19:59:51 +0100

    Create FactoryMethodDemo.java

---
+++ DesignPatterns/FactoryPattern/FactoryMethodDemo.java
@@ -0,0 +1,19 @@
+package org.factoryPattern;
+
+public class FactoryMethodDemo {
+
+    public static void main(String[] args){
+        NotificationFactory factory = getFactory("Email");
+        Notification notification = factory.createNotification();
+        notification.notifyUser();
+    }
+
+    public static NotificationFactory getFactory(String type) {
+        if (type.equalsIgnoreCase("SMS")) {
+            return new SMSNotificationFactory();
+        } else if (type.equalsIgnoreCase("Email")) {
+            return new EmailNotificationFactory();
+        }
+        throw new IllegalArgumentException("Unknown notification type");
+    }
+    }
