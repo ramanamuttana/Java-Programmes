acef2725604a266edf92b763fb056011b553f74b
ramanamuttana <ramanamuttana@gmail.com>
2024-10-30 20:01:39 +0100

    Create SMSNotification.java

---
+++ DesignPatterns/FactoryPattern/SMSNotification.java
@@ -0,0 +1,10 @@
+package org.factoryPattern;
+
+public class SMSNotification implements Notification{
+
+    @Override
+    public void notifyUser() {
+        System.out.println("Sms Notification ");
+
+    }
+}
