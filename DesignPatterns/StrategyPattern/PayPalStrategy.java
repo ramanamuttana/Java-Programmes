cd95e12d44008190048d4ff56a0cbd989fff0aee
ramanamuttana <ramanamuttana@gmail.com>
2024-10-31 09:59:57 +0100

    Create PayPalStrategy.java

---
+++ DesignPatterns/StrategyPattern/PayPalStrategy.java
@@ -0,0 +1,13 @@
+package org.strategy;
+
+public class PayPalStrategy implements PaymentStrategy{
+    private  String email;
+    public PayPalStrategy(String email){
+        this.email=email;
+    }
+
+    @Override
+    public void pay(int amount) {
+        System.out.println("Paid " + amount + " using PayPal account " + email);
+    }
+}
