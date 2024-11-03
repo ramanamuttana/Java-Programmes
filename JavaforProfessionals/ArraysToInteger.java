9b24432268f09273ca1e577a025db7ee2ec168f3
ramanamuttana <ramanamuttana@gmail.com>
2024-11-03 14:12:15 +0100

    Create ArraysToInteger.java

---
+++ JavaforProfessionals/ArraysToInteger.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ArraysToInteger {
+
+    public static void main(String[] args ){
+
+        // convert an array to List
+        Integer[] integerValues= {23,Integer.valueOf(45)};
+
+        List<Integer> values=Arrays.asList(integerValues);
+
+        // convert List values into array of integers
+        Integer[]  listToArray=values.toArray(new Integer[values.size()]);
+
+    }
+}
