f0a7ad9b65dff36f9ca08eaaff47e374e551bb65
Venkata Muttana <ramanamuttana@gmail.com>
2024-11-04 21:46:02 +0100

    Create Book.java

---
+++ JavaCodingProblems/Chapter12/229/Non_Existence/Book.java
@@ -0,0 +1,14 @@
+package org.optional;
+
+public class Book {
+
+    private String name=null;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
