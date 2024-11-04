cb3d0ca9db5501586c677978e908ad03de65044f
Venkata Muttana <ramanamuttana@gmail.com>
2024-11-04 21:45:31 +0100

    Update Non_Existence_DefaultValues.java

--- JavaCodingProblems/Chapter12/229/Non_Existence/Non_Existence_DefaultValues.java
+++ JavaCodingProblems/Chapter12/229/Non_Existence/Non_Existence_DefaultValues.java
@@ -1,13 +1,27 @@
 package org.example;
 
+import java.util.Optional;
+
 public class Non_Existence_DefaultValues {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args){
+        Non_Existence_DefaultValues non_Existence_DefaultValues=new Non_Existence_DefaultValues();
+        Book defaultBook=non_Existence_DefaultValues.nonExistenceDefaultValues();
+        System.out.println(defaultBook.getName()+" Default book incase any book is missing ");
     }
 
-    public void findStatusPrefer(){
+    public Book nonExistenceDefaultValues(){
+        Optional<Book> book= Optional.empty();
 
+        return book.orElseGet(this::computeDefault);
     }
 
+    // adding default book inspite of empty
+    public Book computeDefault(){
+        System.out.println("Default value");
+        Book bs=new Book();
+        bs.setName("Java");
+        return bs;
+    }
 }
+
