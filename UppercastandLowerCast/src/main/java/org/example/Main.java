0c204bc8d2b2db9bbbddf68f4a79daa5a2b6e1b3
Venkata Muttana <ramanamuttana@gmail.com>
2024-11-08 12:51:44 +0100

    Update Main.java

--- UppercastandLowerCast/src/main/java/org/example/Main.java
+++ UppercastandLowerCast/src/main/java/org/example/Main.java
@@ -1,26 +1,28 @@
 package org.example;
-class Machine {
+
+class Machine{
     void on (){
-        System.out.println(" machine is on ");
+        System.out.println(" Machine is on ");
     }
 }
-
-class Tractor extends Machine {
+class Tractor extends Machine{
     @Override
     void on (){
-        System.out.println("Tractor is on ");
+        System.out.println(" Tractor is on ");
     }
 }
 public class Main {
     public static void main(String[] args) {
-        Machine ma=new Tractor();
-        doAction(ma);
-        doAction(new Tractor());
+        Machine ma = new Tractor();
+         doAction(ma);
+         doAction(new Tractor());
+         doAction(new Machine());
+        }
 
+        public static void doAction(Machine maa) {
+            maa.on();
+        }
+        // Tractor is on
+         // Tractor is on 
+         // Machine is on
     }
-
-    public static void doAction(Machine maa){
-        maa.on();
-
-    }
-}
