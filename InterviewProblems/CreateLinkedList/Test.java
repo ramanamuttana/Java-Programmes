c07d0774609e09abcca92af07cd00572e4c69f28
Venkata Muttana <ramanamuttana@gmail.com>
2024-11-10 19:06:58 +0100

    Create Test.java

---
+++ InterviewProblems/CreateLinkedList/Test.java
@@ -0,0 +1,19 @@
+package org.leetcodeproblems;
+
+public class Test {
+
+
+        public static void main(String[] args) {
+            LinkedListDemo list = new LinkedListDemo();
+
+            // Adding values to the linked list
+            list.add(1);
+            list.add(3);
+            list.add(5);
+            list.add(7);
+
+            // Printing the linked list
+            System.out.print("Linked List: ");
+            list.printList();
+        }
+    }
