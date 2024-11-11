92cff430a7a903030590d4041c3398b4b3c31ddf
muttanaramana <muttanaramana@gmail.com>
2024-11-11 07:17:25 +0100

    Update Test.java

--- InterviewProblems/CreateLinkedList/Test.java
+++ InterviewProblems/CreateLinkedList/Test.java
@@ -2,8 +2,8 @@ package org.leetcodeproblems;
 
 public class Test {
 
-
         public static void main(String[] args) {
+                
             LinkedListDemo list = new LinkedListDemo();
 
             // Adding values to the linked list
