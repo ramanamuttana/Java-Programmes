f10e8fdd300c9d3f366f148df1ef2a89d4b2e765
Venkata Muttana <ramanamuttana@gmail.com>
2024-11-16 14:36:03 +0100

    Update Forloop.java

--- javafordummies/Chapters/Chapter1/decisionlopps/Forloop.java
+++ javafordummies/Chapters/Chapter1/decisionlopps/Forloop.java
@@ -1,12 +1,14 @@
 package decisionlopps;
 
-public class Forloop {
+public class EvenandOddNumbers {
 
-	// print the even numbers in a given range
+	// print the even and odd numbers in a given range
 	public static void main(String[] args) {
 
 		for (int i = 0; i < 50; i++) {
 			if (i % 2 == 0) {
+				System.out.println(i);
+			}else{
 				System.out.println(i);
 			}
 		}
