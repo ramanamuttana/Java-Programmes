7950ae056b12eb4ca03dcbfa37ad7a4d2657ff2f
Venkata Muttana <ramanamuttana@gmail.com>
2024-11-16 14:07:12 +0100

    Update Car2.java

--- javafordummies/Chapters/Chapter1/constructors/ExtensionExample/Car2.java
+++ javafordummies/Chapters/Chapter1/constructors/ExtensionExample/Car2.java
@@ -3,14 +3,14 @@ package constructors;
 public class Car2 extends Car {
 	
 	/*
-	 * since we are extending the car class , the variables and methods in the car2 can now  accessed through Object of car
+	 * since we are extending the car class , the variables and methods in the car can now  accessed through Object of car
 	 * now to call a method we can call through Object , but if it having STATIC then we can call it  through CLASSNAME 
-	 * but , here the variable  is within the class and also STATIC  , so we can call directly  variable p 
+	 * but , here the variable  is in within the class and also STATIC  , so we can call directly  variable p 
 	 * 
 	 */
 	
 	public void getWorld() {
-		System.out.println("Hello world car");
+		System.out.println("Hello world car2");
 	}
 
 	public static void main(String[] args) {
