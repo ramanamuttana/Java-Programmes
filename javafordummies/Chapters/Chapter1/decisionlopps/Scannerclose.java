130a0343e3705e4aff7fe4a5f9e7a8a1bc25a652
Venkata Muttana <ramanamuttana@gmail.com>
2024-11-16 14:43:00 +0100

    Update Scannerclose.java

--- javafordummies/Chapters/Chapter1/decisionlopps/Scannerclose.java
+++ javafordummies/Chapters/Chapter1/decisionlopps/Scannerclose.java
@@ -22,4 +22,25 @@ public class Scannerclose {
 		}
 		System.out.println(ans);
 	}
+
+	// Use try-with-resources to handle Scanner closure automatically
+      /*  try (Scanner scan = new Scanner(System.in)) {
+        *    System.out.print("Enter a number: ");
+         *   int n = scan.nextInt();
+        *
+        *     // Determine the category of the number
+        *      if (n % 2 == 1) {
+        *             ans = "oddNumber";
+        *        } else if (n >= 2 && n <= 5) {
+       *           ans = "n may be 2 or 4";
+        *      } else if (n >= 6 && n <= 20) {
+       *         ans = "this must be an even number between 5 and 20";
+        *    } else if (n > 20 && n <= 100) {
+        *       ans = "this must be an even number between 20 and 100";
+        *  } else {
+         *     ans = "number is outside the expected range";
+        *}
+        *}
+*/
+
 }
