0712647a042984cd717a4177a6dea1724c368ca5
Venkata Muttana <ramanamuttana@gmail.com>
2024-11-17 14:25:39 +0100

    Update Buffer.java

--- javafordummies/Chapters/Part2/practicedone/Buffer.java
+++ javafordummies/Chapters/Part2/practicedone/Buffer.java
@@ -2,14 +2,12 @@ package practicedone;
 
 public class Buffer {
 
-			public static void main(String[] args) {
+	public static void main(String[] args) {
 			String s="The value is not found";
 			int start=10;
 			int end=15;
-			char buffer[]= new char[end-start];
-			s.getChars(start, end, buffer, 0);
-			System.out.println(buffer);
-
+			char buffer[] = new char[end - start]; // This creates a char array of size 5
+                        s.getChars(start, end, buffer, 0); // Copies characters from index 10 to 14 (end is exclusive) into buffer
+                        System.out.println(buffer); // Prints the contents of the buffer array
 	}
-
 }
