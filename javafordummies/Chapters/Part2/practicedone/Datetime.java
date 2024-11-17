217cd53ca5680a27fd8e8170124eeeca0c28b6c3
Venkata Muttana <ramanamuttana@gmail.com>
2024-11-17 14:36:26 +0100

    Update Datetime.java

--- javafordummies/Chapters/Part2/practicedone/Datetime.java
+++ javafordummies/Chapters/Part2/practicedone/Datetime.java
@@ -5,13 +5,17 @@ import java.text.*;
 public class Datetime {
 
 public static void main(String[] args) {
-Date date=new Date();
-System.out.println(date.toString());
-int c=date.getMonth();
-c+=1;
-System.out.println(+c);
-SimpleDateFormat form= new SimpleDateFormat("E dd/mm/yyyy HH-mm-ss");
-System.out.println(form.format(date));
+         Date date=new Date();
+          System.out.println(date.toString());
+           int c=date.getMonth();
+            c+=1;
+           System.out.println(+c);
+           SimpleDateFormat form= new SimpleDateFormat("E dd/mm/yyyy HH-mm-ss");
+            System.out.println(form.format(date));
 }
+  // sample output
+  //Default date: Sun Nov 17 14:30:15 CET 2024
+//Current month (1-based): 11
+//Formatted date: Sun 17/11/2024 14-30-15
 
 }
